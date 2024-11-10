07921b3fb753935ab1ada02f30d8aa4d450cf415
kimchanho97 <nh0903@pusan.ac.kr>
2024-11-11 04:32:26 +0900

    feat(ConvenienceStoreController): 컨트롤러 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,18 @@
 package store;
 
+import store.controller.ConvenienceStoreController;
+import store.domain.ConvenienceStore;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // new ConvenienceStoreService( new ConvenienceStore)
+
+        ConvenienceStore store = new ConvenienceStore();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        new ConvenienceStoreController(store, inputView, outputView).run();
     }
 }
